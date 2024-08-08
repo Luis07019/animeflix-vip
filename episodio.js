41af0f0241ea99b237a7292cebbbe0fce41a266c
Luis07019 <100967438+Luis07019@users.noreply.github.com>
2024-08-08 16:45:35 -0500

    episodio.js

--- episodio.js
+++ episodio.js
@@ -1,1 +1,9 @@
+let contenidopro = [
+  post: "data:post.id",
+  home:"data:blog.homepageUrl",
+  id: "${contenido}",
+let episodios = `<div class="cv-eps-id mb-8" expr:data-current-id='data:post.id' expr:data-home='data:blog.homepageUrl' expr:data-id='_'></div>`;
+let contenido = episodios;
+let Epro = (contenido);
 
+document.write(Epro)
